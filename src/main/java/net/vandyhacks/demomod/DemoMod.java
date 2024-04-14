7f897c4f6d9bc815d0ed3d1ebb33bea7dc017b12
natedoesthings <nathanaeldtesfaye@gmail.com>
2024-04-14 00:25:07 -0500

    initial commit

--- src/main/java/net/vandyhacks/demomod/DemoMod.java
+++ src/main/java/net/vandyhacks/demomod/DemoMod.java
@@ -9,6 +9,7 @@ public class DemoMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "demomod";
     public static final Logger LOGGER = LoggerFactory.getLogger("demomod");
 
 	@Override
